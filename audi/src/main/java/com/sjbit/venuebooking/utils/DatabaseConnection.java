b83ebc21b34eb8c9d1fad11a83ec98fa145fc9b2
Rohan K R <163729562+rohanraj006@users.noreply.github.com>
2025-03-19 23:04:17 +0530

    Update DatabaseConnection.java

--- audi/src/main/java/com/sjbit/venuebooking/utils/DatabaseConnection.java
+++ audi/src/main/java/com/sjbit/venuebooking/utils/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:postgresql://localhost:5432/sjbit";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "rohanraj";
+    private static final String PASSWORD = "--";
 
     static {
         try {
